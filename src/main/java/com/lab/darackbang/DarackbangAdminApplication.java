a9fc568ec4f48ad704932f69a3bf9d286a3b9f34
klaatus <klaatus@msn.com>
2024-09-06 21:01:10 +0900

    다락방어디민 소스코드 추가

---
+++ src/main/java/com/lab/darackbang/DarackbangAdminApplication.java
@@ -0,0 +1,13 @@
+package com.lab.darackbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarackbangAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarackbangAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lab/darackbang/DarackbangAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.darackbang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarackbangAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
